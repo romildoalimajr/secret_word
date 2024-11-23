d42b3b2970202c321152f9b80a985269963fb651
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-22 22:42:05 -0300

    Estilizando a tela inicial.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
-      <h2>Secret World</h2>
+      <StartScreen />
     </div>
   );
 }
