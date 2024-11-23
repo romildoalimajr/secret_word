d7e9a32d6826a51757c4e7790574de9046dd05d9
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-22 21:43:56 -0300

    Configurando o ambiente de trablaho.

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja', 'Jaca', 'Acerola'],
+    corpo: ['Braço', 'Perna', 'Cérebro', 'Pescoço', 'Olhos', 'Ouvidos', 'Nariz'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete', 'Memória', 'Chipset'],
+    programação: ['Linguagem', 'Framework', 'JavaScript', 'React', 'React-native', 'Java'],
+    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo', 'Frango', 'Macarrão', 'Legumes'],
+    países: ['Brasil', 'Argentina', 'Inglaterra', 'Chile', 'Equador', 'Australia', 'China', 'Portugal']
+};
